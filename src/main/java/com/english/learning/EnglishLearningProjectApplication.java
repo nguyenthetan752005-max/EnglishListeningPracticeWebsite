2194166f2d02ad5ec6aeb86f4fc0e05578fe0d02
nillmen <23110151@student.hcmute.edu.vn>
2026-03-23 15:50:13 +0700

    Tam fixed error port

--- src/main/java/com/english/learning/EnglishLearningProjectApplication.java
+++ src/main/java/com/english/learning/EnglishLearningProjectApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.util.Properties;
+import java.util.Map;
+
 @SpringBootApplication
 public class EnglishLearningProjectApplication {
 
@@ -14,7 +20,39 @@ public class EnglishLearningProjectApplication {
     private Environment env;
 
     public static void main(String[] args) {
-        SpringApplication.run(EnglishLearningProjectApplication.class, args);
+
+        SpringApplication app = new SpringApplication(EnglishLearningProjectApplication.class);
+
+        Properties properties = new Properties();
+        setPort(properties);
+
+        app.run(args);
+    }
+
+    private static void setPort(Properties properties) {
+        try (InputStream input = EnglishLearningProjectApplication.class.getClassLoader()
+                .getResourceAsStream("application.properties")) {
+            properties.load(input);
+            int port = Integer.parseInt(properties.getProperty("server.port", "8080"));
+            if (!isPortAvailable(port)) {
+                System.out.println("Cổng " + port + " đã bị sử dụng. Thử dùng cổng khác ...");
+                while (!isPortAvailable(port)) {
+                    port++;
+                }
+                System.out.println("Đã tìm thấy cổng " + port + " trống. Chương trình sẽ sử dụng cổng này!");
+                System.setProperty("server.port", String.valueOf(port));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean isPortAvailable(int port) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     @EventListener(ApplicationReadyEvent.class)
