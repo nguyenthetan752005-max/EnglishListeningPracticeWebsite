2ff427939627c723dc2dd5e1719db13e95ae8638
Luu Quang Tien <23110157@student.hcmute.edu.vn>
2026-03-27 12:05:18 +0700

    save trước khi merge

--- src/main/java/com/english/learning/security/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/english/learning/security/CustomOAuth2SuccessHandler.java
@@ -12,7 +12,6 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.Optional;
 import java.util.UUID;
 
